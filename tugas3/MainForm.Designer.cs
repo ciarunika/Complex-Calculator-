5f4c3c1bbfc3e175b9d7bf9d37f0b13050e37700
Yogi <126434742+ciarunika@users.noreply.github.com>
2023-03-13 17:58:56 +0700

    Add files via upload

---
+++ tugas3/MainForm.Designer.cs
@@ -0,0 +1,302 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Yenyin
+ * Date: 13/03/2023
+ * Time: 16:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace tugas3
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(130, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Bilangan pertama";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 90);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(130, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Bilangan kedua";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(13, 152);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Hasil";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label4.Location = new System.Drawing.Point(72, 50);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(30, 23);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "+ j";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(72, 116);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(30, 23);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "+ j";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.radioButton4);
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+			this.groupBox1.Location = new System.Drawing.Point(189, 23);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(150, 210);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Operasi";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(88, 178);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(56, 21);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "Exit";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(6, 178);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(56, 21);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Reset";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(37, 148);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 24);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Hitung";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.Location = new System.Drawing.Point(18, 118);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(66, 24);
+			this.radioButton4.TabIndex = 3;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "Bagi";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton4CheckedChanged);
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(18, 85);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(75, 24);
+			this.radioButton3.TabIndex = 2;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Kali";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(18, 55);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(75, 24);
+			this.radioButton2.TabIndex = 1;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Kurang";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2CheckedChanged);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(18, 26);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(94, 24);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Tambah";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1CheckedChanged);
+			// 
+			// label6
+			// 
+			this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label6.Location = new System.Drawing.Point(12, 183);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(58, 23);
+			this.label6.TabIndex = 6;
+			// 
+			// label7
+			// 
+			this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label7.Location = new System.Drawing.Point(110, 183);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(57, 23);
+			this.label7.TabIndex = 7;
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label8.Location = new System.Drawing.Point(75, 183);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(30, 23);
+			this.label8.TabIndex = 8;
+			this.label8.Text = "+ j";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(12, 49);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(49, 20);
+			this.textBox1.TabIndex = 9;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Location = new System.Drawing.Point(110, 50);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(49, 20);
+			this.textBox2.TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox3.Location = new System.Drawing.Point(13, 116);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(49, 20);
+			this.textBox3.TabIndex = 11;
+			this.textBox3.TextChanged += new System.EventHandler(this.TextBox3TextChanged);
+			// 
+			// textBox4
+			// 
+			this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox4.Location = new System.Drawing.Point(110, 115);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(49, 20);
+			this.textBox4.TabIndex = 12;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(351, 245);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "tugas3";
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ tugas3/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tugas3{
+
+	public partial class MainForm : Form{
+		
+		public MainForm(){
+			InitializeComponent();
+		}
+		
+		void TextBox3TextChanged(object sender, EventArgs e){
+		}
+		void RadioButton1CheckedChanged(object sender, EventArgs e){
+		}
+		void RadioButton2CheckedChanged(object sender, EventArgs e){
+		}
+		void RadioButton3CheckedChanged(object sender, EventArgs e){
+		}
+		void RadioButton4CheckedChanged(object sender, EventArgs e){
+		}
+		
+		void Button1Click(object sender, EventArgs e){
+		
+			double r1,r2,i1,i2,real,imajiner;
+			
+			r1 = Convert.ToDouble(textBox1.Text);
+			r2 = Convert.ToDouble(textBox3.Text);
+			i1 = Convert.ToDouble(textBox2.Text);
+			i2 = Convert.ToDouble(textBox4.Text);
+			
+			if (radioButton1.Checked == true){
+			
+				real = (r1+r2);
+				imajiner = (i1+i2);
+				
+				label6.Text = real.ToString();
+				label7.Text = imajiner.ToString();
+			}
+			
+			else if (radioButton2.Checked == true){
+				
+				real = (r1-r2);
+				imajiner = (i1-i2);
+				
+				label6.Text = real.ToString();
+				label7.Text = imajiner.ToString();
+			}
+			
+			else if(radioButton3.Checked == true){
+				
+				real = (r1*r2)-(i1*i2);
+				imajiner = (r1*i2 )+(i1*r2);
+				
+				label6.Text = real.ToString();
+				label7.Text = imajiner.ToString();
+			}
+			
+			else if (radioButton4.Checked == true){
+				
+				real = (r1*r2+i1*i2)/(r2*r2+i2*i2);
+				imajiner = (r2*i1-r1*i2)/(r2*r2+i2*i2);
+				
+				label6.Text = real.ToString();
+				label7.Text = imajiner.ToString();
+			}
+		}
+		
+		void Button2Click(object sender, EventArgs e){
+			
+			textBox1.Clear();
+			textBox2.Clear();
+			textBox3.Clear();
+			textBox4.Clear();
+			label6.Text = String.Empty;
+			label7.Text = String.Empty;
+			
+			radioButton1.Checked  = false ;
+			radioButton2.Checked  = false ;
+			radioButton3.Checked  = false ;
+			radioButton4.Checked  = false ;
+		}
+		
+		void Button3Click(object sender, EventArgs e){
+			Environment.Exit(0);
+		}
+	}
+}
